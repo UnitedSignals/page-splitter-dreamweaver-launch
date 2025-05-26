835009d55cc6675f1bbc5cf16d54cf8c74b3e47e
Jan Roesner <jan@roesner.it>
2025-05-26 11:44:18 +0200

    Solved <spa redirect problem by duplicating index as 404 page

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { resolve } from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: '/page-splitter-dreamweaver-launch/',
   server: {
@@ -12,12 +12,20 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        // ⚠️ das hier erzeugt automatisch eine 404.html aus index.html
+        '404': resolve(__dirname, 'index.html'),
+      },
     },
   },
 }));
