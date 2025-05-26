171d03f04921bd91cb29664ecf4748be413ea0f5
Jan Roesner <jan@roesner.it>
2025-05-26 11:39:49 +0200

    Added base path to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/page-splitter-dreamweaver-launch/',
   server: {
     host: "::",
     port: 8080,
