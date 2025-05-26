74204fcf46c842725f35e21da50df6b9de5c40b4
Jan Roesner <jan@roesner.it>
2025-05-26 14:05:49 +0200

    Trying to get BrowserRouter corrected

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -17,13 +16,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/page-splitter-dreamweaver-launch">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/landing1" element={<Landing1 />} />
           <Route path="/landing2" element={<Landing2 />} />
           <Route path="/landing3" element={<Landing3 />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
